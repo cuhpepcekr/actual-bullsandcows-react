e21ba3034005d953260759b5d8277b662d4fb0a7
cuhpepcekr <lintogm@gmail.com>
2020-07-12 16:18:27 +0900

    회원가입 페이지 추가.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import SignIn from "./views/SignIn";
 import Home from "./views/Home";
+import SignUp from "./views/SignUp";
+import SignIn from "./views/SignIn";
 
 function App() {
   return (
     <BrowserRouter>
       <Route path="/" component={Home} exact />
+      <Route path="/sign-up" component={SignUp} exact />
       <Route path="/sign-in" component={SignIn} exact />
     </BrowserRouter>
   );
